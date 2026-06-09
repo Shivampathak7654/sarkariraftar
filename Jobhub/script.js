22c9177df447c9e4a88c8e43c9a700ecb62f52d0
Shivampathak7654 <indusytsectorshivamsc@gmail.com>
2026-06-09 12:15:48 +0530

    Add files via upload

---
+++ Jobhub/script.js
@@ -0,0 +1,5 @@
+function unlockPDF() {
+  alert("VIP Notes Unlock ho rahe hain... Kripya 2 second rukein!");
+  document.getElementById("lock-screen").style.display = "none";
+  document.getElementById("download-screen").style.display = "block";
+}
